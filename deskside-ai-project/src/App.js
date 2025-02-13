a30a393986d2eebce16ddd988dc7561cf1959dc6
Krunal Parmar <krunal97parmar@gmail.com>
2025-02-12 22:51:42 -0500

    add new hire email component

--- deskside-ai-project/src/App.js
+++ deskside-ai-project/src/App.js
@@ -1,9 +1,21 @@
 import IncidentReportTemplate from "./components/IncidentReportTemplate";
+import NewHireLaptopSetup from "./components/NewHireLaptopSetup";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <IncidentReportTemplate />
+    <div className="app">
+      <h1 className="main-heading">
+        AI-Powered DeskSide Management – Transforming IT Support - Prototype
+      </h1>
+      <div className="content">
+        <div className="sub-component">
+          <IncidentReportTemplate />
+        </div>
+        <div className="sub-component">
+          <NewHireLaptopSetup />
+        </div>
+      </div>
     </div>
   );
 }
